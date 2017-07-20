c183f672fa22fbd554736248a30234f42b03f39a
larachicharo <laralacesar@gmail.com>
2017-07-20 02:53:23 -0500

    Printing welcome screen from file.

---
+++ welcome.c
@@ -0,0 +1,52 @@
+#include <curses.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void print_file(char *filename, unsigned int x, unsigned int y) {
+	FILE *fp;
+	if ((fp = fopen(filename, "r")) != NULL) {
+		char final_str[count_chars(fp)];
+		size_t i = 0;
+		int c;
+		for (c = getc(fp); c != EOF; c = getc(fp))
+			final_str[i++] = c;
+		printf("%s", final_str);
+		mvprintw(y, x, final_str);
+		fclose(fp);
+	} else {
+		fprintf(stderr, "cant open file: %s", filename);
+		exit(1);
+	}
+}
+
+int count_chars(FILE *fp) {
+	unsigned int chars = 0;
+	int c;
+	for (c = getc(fp); c != EOF; c = getc(fp))
+		chars++;
+	if (fseek(fp, 0L, SEEK_SET) != 0) {
+		printf("Repositioning error.");
+		exit(1);
+	}
+	return chars;
+}
+
+void print_title() {
+	#define FFLAPPY_SRC "res/title.txt"
+	print_file(FFLAPPY_SRC, 0, 0);
+}
+
+
+int main (void) {
+	initscr();
+	noecho();
+	cbreak();
+	curs_set(FALSE);
+	
+	print_title();
+	
+	refresh();
+	sleep(2);
+	endwin();
+}
