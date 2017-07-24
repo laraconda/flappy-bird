518929fb1ecfc29fbc64ae2b11e843e4027659bb
larachicharo <laralacesar@gmail.com>
2017-07-24 01:13:22 -0500

    As soon as an obstacle surpases the window boundaries, its relocated behind the last obstacle.

--- game.c
+++ game.c
@@ -11,29 +11,46 @@
 #define SPEED SECOND_IN_MICROSECONDS / FPS
 
 #define OBSTACLE_WIDTH 4
+#define OBSTACLE_SPACING 5
 
 // simplified version of an obstacle.
 struct obstacle {
-	unsigned char width;
-	unsigned char y;
+	char width;
+	char y;
 };
 
 struct pair_of_obstacles {
-	unsigned char x;
+	char x;
 	struct obstacle obs_a;
 	struct obstacle obs_b;
 
 };
 
 unsigned char alive = 1;
-struct pair_of_obstacles pair;
+unsigned int n_obstacles;
+struct pair_of_obstacles* pairs;
 
-void init_obstacles(void) {
+void init_obstacle(unsigned int i) {
 	struct obstacle obs_a = {OBSTACLE_WIDTH, 0};
 	struct obstacle obs_b = {OBSTACLE_WIDTH, LINES - 1};
-	pair.x = 0;
-	pair.obs_a = obs_a;
-	pair.obs_b = obs_b;
+	pairs[i].x = (OBSTACLE_WIDTH + OBSTACLE_SPACING) * i * (-1);
+	pairs[i].obs_a = obs_a;
+	pairs[i].obs_b = obs_b;
+}
+
+void init_obstacles(void) {
+	unsigned int i;
+	n_obstacles = 10;
+	pairs = malloc(
+		n_obstacles * sizeof(struct pair_of_obstacles));
+	for (i=0; i<n_obstacles; i++) {
+		init_obstacle(i);
+	}
+}
+
+void insert_obstacle_behind_its_next(unsigned int i) {
+	pairs[i].x = pairs[(i - 1 + n_obstacles) % n_obstacles].x -
+		(OBSTACLE_SPACING + OBSTACLE_WIDTH);
 }
 
 char * get_obstacle_repr(void) {
@@ -46,20 +63,29 @@ char * get_obstacle_repr(void) {
 	return strcpy(s, s_local);
 }
 
-void advance_pair_of_obstacles(void) {
-	pair.x += 1;
+unsigned char has_obstacle_finnished_its_track(
+	struct pair_of_obstacles obs) {
+	return (obs.x >= COLS);
+}
+
+void advance_obstacles(void) {
+	unsigned int i;
+	for (i=0; i<n_obstacles; i++) {
+		if (has_obstacle_finnished_its_track(pairs[i])) {
+			insert_obstacle_behind_its_next(i);
+		} else
+			pairs[i].x += 1;
+	}
 }
 
 void periodic_events(void) {
-	advance_pair_of_obstacles();
+	advance_obstacles();
 }
 
 void *listen_controller(void * arg) {
 	while(alive) {
-		if (getch() == SPACEBAR) {
-			pair.obs_a.y += 1;
-			//pair.obs_b.y += 1;
-		}
+		if (getch() == SPACEBAR)
+			advance_obstacles();
 	}
 
 	pthread_exit(NULL);
@@ -75,15 +101,25 @@ void init_controller_listener(void) {
 	}
 }
 
-void print_screen(void) {	
-	clear();
-
+void print_obstacle(unsigned int i) {
 	char * obs_repr = get_obstacle_repr();
-	mvprintw(pair.obs_a.y, pair.x, obs_repr);
-	mvprintw(pair.obs_b.y, pair.x, obs_repr);
+	mvprintw(pairs[i].obs_a.y, pairs[i].x, obs_repr);
+	mvprintw(pairs[i].obs_b.y, pairs[i].x, obs_repr);
 	
 	free(obs_repr);
 	refresh();
+}
+
+void print_obstacles() {
+	unsigned int i;
+	for (i=0; i<n_obstacles; i++)
+		print_obstacle(i);
+}
+
+
+void print_screen(void) {	
+	clear();
+	print_obstacles();
 }
 
 void start_game(void) {
@@ -96,4 +132,5 @@ void start_game(void) {
 	}
 
 	pthread_exit(NULL);
+	free(pairs);
 }
