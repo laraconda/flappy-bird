e4d5291697241bf664de73a1708f07aebecbb8f0
larachicharo <laralacesar@gmail.com>
2017-07-23 21:45:58 -0500

    Creating parallel (preliminar) obstacles.

--- game.c
+++ game.c
@@ -1,6 +1,8 @@
 #include <pthread.h>
 #include <curses.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 
 #include "keys.h"
 
@@ -8,32 +10,55 @@
 #define SECOND_IN_MICROSECONDS 1000000
 #define SPEED SECOND_IN_MICROSECONDS / FPS
 
-// using point as a simplified version of an obstacle.
-struct point {
-	unsigned char x;
+#define OBSTACLE_WIDTH 4
+
+// simplified version of an obstacle.
+struct obstacle {
+	unsigned char width;
 	unsigned char y;
 };
 
-unsigned char alive = 1;
-struct point po = {0, 0};
+struct pair_of_obstacles {
+	unsigned char x;
+	struct obstacle obs_a;
+	struct obstacle obs_b;
 
-char * get_point_string(void) {
-	char * s = "@";
-	return s;
+};
+
+unsigned char alive = 1;
+struct pair_of_obstacles pair;
+
+void init_obstacles(void) {
+	struct obstacle obs_a = {OBSTACLE_WIDTH, 0};
+	struct obstacle obs_b = {OBSTACLE_WIDTH, LINES - 1};
+	pair.x = 0;
+	pair.obs_a = obs_a;
+	pair.obs_b = obs_b;
 }
 
-void advance_point(void) {
-	po.x += 1;
+char * get_obstacle_repr(void) {
+	char *cell = "@";
+	char s_local[OBSTACLE_WIDTH];
+	int i;
+	for (i=0; i<OBSTACLE_WIDTH; i++)
+		s_local[i] = *cell;
+	char *s = malloc(OBSTACLE_WIDTH * sizeof(char));
+	return strcpy(s, s_local);
+}
+
+void advance_pair_of_obstacles(void) {
+	pair.x += 1;
 }
 
 void periodic_events(void) {
-	advance_point();
+	advance_pair_of_obstacles();
 }
 
 void *listen_controller(void * arg) {
 	while(alive) {
 		if (getch() == SPACEBAR) {
-			po.y += 1;
+			pair.obs_a.y += 1;
+			//pair.obs_b.y += 1;
 		}
 	}
 
@@ -53,12 +78,16 @@ void init_controller_listener(void) {
 void print_screen(void) {	
 	clear();
 
-	mvprintw(po.y, po.x, get_point_string());
+	char * obs_repr = get_obstacle_repr();
+	mvprintw(pair.obs_a.y, pair.x, obs_repr);
+	mvprintw(pair.obs_b.y, pair.x, obs_repr);
 	
+	free(obs_repr);
 	refresh();
 }
 
 void start_game(void) {
+	init_obstacles();
 	init_controller_listener();
 	while(alive) {
 		print_screen();
